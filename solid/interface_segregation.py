3550fede8e102f4accbbb275c7214e47a017ce36
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-07-25 20:09:45 +0100

    Another interface segregation example

--- solid/interface_segregation.py
+++ solid/interface_segregation.py
@@ -1,25 +1,51 @@
-class PlaySongs:
+from abc import abstractmethod
 
-    def __init__(self, title):
+
+class PlaySongsLyrics:
+
+    @abstractmethod
+    def sing_lyrics(self):
+        pass
+
+
+class PlaySongsMusic:
+
+    @abstractmethod
+    def play_guitar(self):
+        pass
+
+    @abstractmethod
+    def play_drums(self):
+        pass
+
+
+class PlayInstrumentalSong(PlaySongsMusic):
+
+    def play_drums(self):
+        print("Bum-bum-bum")
+
+    def play_guitar(self):
+        print("Some guitar solo*")
+
+
+class PlayRockSong(PlaySongsMusic, PlaySongsLyrics):
+    def __init__(self, title: str):
         self.title = title
 
     def get_upper_case_title(self):
         return self.title.upper()
 
-
-class PlayRockSongs(PlaySongs):
-
     def play_guitar(self):
         print("Heavy metal guitar solo*")
 
     def sing_lyrics(self):
-        print("We will, we will rock you!")
+        print("We will, we will rock you")
 
     def play_drums(self):
         print("Bum-bum-bum")
 
 
 if __name__ == '__main__':
-    rock_songs = PlayRockSongs('We will rock you')
+    rock_songs = PlayRockSong('We will rock you')
     rock_songs.sing_lyrics()
     print(rock_songs.get_upper_case_title())
